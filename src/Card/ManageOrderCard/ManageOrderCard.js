04b254077601ffc36eb45b7a26b4dd5e0a77072b
mhrazib121 <mhrazib121@gmail.com>
2022-09-21 21:45:00 +0600

    update

--- src/Card/ManageOrderCard/ManageOrderCard.js
+++ src/Card/ManageOrderCard/ManageOrderCard.js
@@ -1,9 +1,36 @@
 import React from 'react';
 
-const ManageOrderCard = (props) => {
+const ManageOrderCard = ({ order }) => {
+    // console.log(order)
+    const { _id, firstName, address, myOrder, status } = order;
+
     return (
         <div>
             <div className='card-design shadow-2xl m-3'>
+                <div className='p-3 '>
+                    <h3 className='text-md font-bold'> No:{_id}</h3>
+                    <h3> {firstName}</h3>
+                    <h3> Address:{address}</h3>
+                    <div className='flex'>
+                        <h3> Total product:<span className='text-color'> {myOrder.length}</span></h3>
+                        <p className='ms-auto text-md text-color'> {status}</p>
+                    </div>
+                </div>
+                <div >
+                    {
+                        myOrder.map(e => <div className='p-2' key={e.id} e={e}>
+                            {/* {const quan =(e?.cart?.quantity)} */}
+                            <h3>Items:</h3>
+                            <div className='flex justify-between'>
+                                <h3>{e.cart.productName}</h3>
+                                <p className='text-color'>{e.cart.quantity}</p>
+                            </div>
+                            {/* <h3 value={valueSet}></h3> */}
+                        </div>)
+                    }
+                </div>
+            </div>
+            {/* <div className='card-design shadow-2xl m-3'>
                 <div className='p-3'>
                     <img className='img-edit' src={props?.order?.product?.img} alt="" />
                 </div>
@@ -17,7 +44,7 @@ const ManageOrderCard = (props) => {
                         <button className='btn-all ms-auto' onClick={() => props.updateOrderStatus(props?.order?._id)}> Update </button>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </div>
     );
 };
