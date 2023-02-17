e7e1c92f2a764c819fc51e710dfec6610821f3b7
razib121 <mhrazib121@gmail.com>
2023-02-17 20:47:52 +0600

    update

--- src/Notify/AddToCard/AddToCard.js
+++ src/Notify/AddToCard/AddToCard.js
@@ -3,10 +3,6 @@ import React from 'react';
 const AddToCard = () => {
     return (
         <div id='customAlert'>
-            {/* <div id='alert-box'>
-                <h1>dlskjflkjsdl { }</h1>
-                <button id='alert-close'> ok </button>
-            </div> */}
         </div>
     );
 };
